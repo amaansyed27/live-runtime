4ee862f915fae80a2b2c2e25b092e0475ea42adf
Amaan Syed <amaansyed27@gmail.com>
2026-06-18 20:45:59 +0530

    Load voice settings stylesheet

--- apps/dashboard/src/main.tsx
+++ apps/dashboard/src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
-import { SettingsResetInjector as SettingsDataPanel } from "./components/SettingsResetInjector";
+import { SettingsResetInjector } from "./components/SettingsResetInjector";
 import "./styles.css";
 import "./scrollbars.css";
 import "./skills.css";
+import "./voice-settings.css";
 import "./alpha-polish.css";
 import "./components/companion-window.css";
 import "./components/companion-final-polish.css";
@@ -12,6 +13,6 @@ import "./components/companion-final-polish.css";
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-    <SettingsDataPanel />
+    <SettingsResetInjector />
   </React.StrictMode>
 );
