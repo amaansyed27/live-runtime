abacac5a12cbdfd4141583fd0107a440e0ab874c
Amaan Syed <amaansyed27@gmail.com>
2026-06-18 18:26:07 +0530

    Stop mounting companion DOM controller

--- apps/dashboard/src/main.tsx
+++ apps/dashboard/src/main.tsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
-import { CompanionModeController as CompanionDockPanel } from "./components/CompanionModeController";
 import { SettingsResetInjector as SettingsDataPanel } from "./components/SettingsResetInjector";
 import "./styles.css";
 import "./scrollbars.css";
 import "./skills.css";
 import "./alpha-polish.css";
+import "./components/companion-window.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
     <SettingsDataPanel />
-    <CompanionDockPanel />
   </React.StrictMode>
 );
