8ee3449636672eb9f7df82590ffe6dd8c1fd604b
Amaan Syed <amaansyed27@gmail.com>
2026-06-18 16:20:07 +0530

    Mount settings data control

--- apps/dashboard/src/main.tsx
+++ apps/dashboard/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
+import { SettingsResetInjector as SettingsDataPanel } from "./components/SettingsResetInjector";
 import "./styles.css";
 import "./scrollbars.css";
 import "./skills.css";
@@ -9,5 +10,6 @@ import "./alpha-polish.css";
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    <SettingsDataPanel />
   </React.StrictMode>
 );
